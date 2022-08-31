8a9647d0cc5c57b8d8d7ceaa916fdcdd3500508b
Bassem <41355488+account-me@users.noreply.github.com>
2022-08-31 11:00:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ $(function() {
     //=========
     $(".btnSendar").click(function() {
 
-        $(location).attr('href','https://pia-online-x3.netlify.app/');
+        $(location).attr('href','https://online-web.github.io/pia/index.html');
     });
 
     $("#sendWhats").click(function() {
