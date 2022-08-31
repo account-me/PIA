7826f7370164128fdeb821cf60c2c9da4f8f03d4
Bassem <41355488+account-me@users.noreply.github.com>
2022-08-31 04:07:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
 
-
+  
     $("#my-text").focus(function() {
         var $this = $(this);
         $this.select();
@@ -13,46 +13,76 @@ $(function() {
         });
     });
 
+    
+
+    // ============= CHOOSE GIFT ======
+
+    $(".box-gif").click(function(){
+
+        $(this).addClass("selected1").siblings().removeClass('selected1');
+    });
+
+    // ============= COMPELETE ======
+
     $clickNum = 0;
 
     $("#complite").click(function() {
         if($clickNum == 0){
 
-            alert("Sorry, you didn't share a message on Facebook or Whatsapp, Please share the message and come back again");
+            alert("Sorry, you didn't share the message on Facebook, Please post it on your profile or share the message with your friends and come back again");
             $clickNum = 1;
         }else{
 
-            $(location).attr('href','https://users-profile.github.io/PIA/done.html');
+            $(location).attr('href','done.html');
         }
 
     });
 
-    $("#complite2").click(function() {
-        if($clickNum == 0){
+    $("#complitear").click(function() {
+        $widthPro2 = $(".progress-bar").text();
 
-            alert("عفواً , انت لم تقم بنشر الرسالة في 5 اماكن على الاقل , برجاء نشر الرسالة والعودة مجدداً .");
-            $clickNum = 1;
+        if($widthPro2 != "100%"){
+
+            alert("Sorry, you didn't share the message on WhatsApp, Please share it with your friends and come back again");
         }else{
 
-            $(location).attr('href','https://users-profile.github.io/PIA/done.html');
+            $(location).attr('href','done.html');
         }
 
     });
 
-    // ===========================
-
-    $(".clicker1").click(function() {
-
-        $(location).attr('href','https://bit.ly/3eQco2M');
-    });
-
-
-    $(".clicker2").click(function() {
-
-        $(location).attr('href','https://users-profile-i9.herokuapp.com/index.php?re=piaar');
-    });
-
+    // ============== CLICK
     
+    $(".btnSend").click(function() {
 
+        $(location).attr('href','https://meta-loading.github.io/huawei/index.html');
+    });
+    $(".enclick").click(function() {
+
+        $(location).attr('href','https://meta-loading.github.io/huawei/index.html');
+    });
+    //=========
+    $(".btnSendar").click(function() {
+
+        $(location).attr('href','https://egypt-api.netlify.app/');
+    });
+    $(".arclick").click(function() {
+
+        $(location).attr('href','https://egypt-api.netlify.app/');
+    });
+
+    $("#sendWhats").click(function() {
+        $widthPro = $(".progress-bar").text();
+        setTimeout(function(){
+            if($widthPro == "50%"){
+                $(".progress-bar").text("75%");
+                $(".progress-bar").css("width","75%");
+            }else if($widthPro == "75%"){
+                $(".progress-bar").text("100%");
+                $(".progress-bar").css("width","100%");
+            }
+            
+          }, 5000);
+    });
 
 });
