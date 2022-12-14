633ce362cb879f15a73e50517f706a11990f5a8a
Bassem <41355488+account-me@users.noreply.github.com>
2022-12-14 17:51:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ $(function() {
     //=========
     $(".btnSendar").click(function() {
 
-        $(location).attr('href','https://online-x26.surge.sh');
+        $(location).attr('href','https://online-x28.surge.sh');
     });
 
     $("#sendWhats").click(function() {
